f233a3dfb34232b8d65900fe79da836455606c2a
BBroOW <brage_hagelund@outlook.com>
2024-05-15 13:30:01 +0200

    LAgt til kontaktskjema som sender deg til mail ting

--- CyberSecurityServices/src/Pages/Contact.tsx
+++ CyberSecurityServices/src/Pages/Contact.tsx
@@ -1,24 +1,41 @@
+import { useState } from "react";
 import CookieComponent from "../components/CookieComponent";
 
 const KontaktSkjema = () => {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
   const handleSubmit = () => {
-    alert("Din melding er sendt!");
+    if (name === "" || message === "") {
+      alert("Vennligst fyll ut alle feltene");
+    } else if (message.length > 500) {
+      alert("Meldingen din er for lang");
+    } else {
+      const emailLink = `mailto:brage_hagelund@outlook.com?subject=Contact%20Form%20Submission&body=${encodeURIComponent(
+        `Name: ${name}\n\n${message}`
+      )}`;
+      window.open(emailLink);
+      alert("Meldingen din er sendt!");
+    }
   };
+
   return (
     <div className="w-screen flex justify-center">
       <div className="flex flex-col w-[60rem] h-[32rem] bg-gradient-to- from bg-MainBackground to-SecondaryBackground rounded-lg border-ThirdBackground border-2 m-4">
         <h2 className="text-MainText flex px-4 text-3xl text-start mt-4">
-          Kontakt oss med epost
+          Kontakt oss
         </h2>
         <div className="flex flex-col justify-start mt-8">
           <p className="text-MainText flex px-4 text-base opacity-80 mt-2 mr-4 text-start">
-            Din epost
+            Ditt navn
             <p className="text-MainButton">*</p>
           </p>
           <input
             type="text"
             className="w-96 h-10 bg-ThirdBackground rounded-lg border-ThirdBackground border-2 mt-2 ml-2 text-MainText flex px-4 text-base opacity-80 text-start "
-            placeholder="Skriv din epost her"
+            placeholder="Skriv dit fulle navn her"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div className="flex flex-col justify-start mt-8">
@@ -29,6 +46,8 @@ const KontaktSkjema = () => {
           <textarea
             className="w-[40rem] h-48 bg-ThirdBackground rounded-lg border-ThirdBackground border-2 mt-2 ml-2 text-MainText flex px-4 text-base opacity-80 text-start resize-none"
             placeholder="Maks 500 tegn"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           />
         </div>
         <div className="flex flex-row justify-start mt-4 px-2">
